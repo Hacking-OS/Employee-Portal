c02e0df397abb3bf3213c3ebfe88e469eaf48b1b
Mufaddal_Beyondvison <mufaddal.beyondvison@gmail.com>
2025-02-13 14:45:25 +0500

    Issue Fix#11009

--- src/app/Shared-Module/Services/shared-end-point.service.ts
+++ src/app/Shared-Module/Services/shared-end-point.service.ts
@@ -16,9 +16,11 @@ export class SharedEndPointService extends RequestHandlerService {
   GetApiResponse<Response, OtherParams = any>(apiPath: string, obj: OtherParams): Observable<Response> {
     const url = `${environment.baseUrl}/${apiPath}`;  // Construct the URL dynamically
 
-    const headers = new HttpHeaders({
+    const headers = {
+      headers : new HttpHeaders({
       'Authorization': `Bearer ${this.getAuthService().getToken()}`
-    });
+    })
+  };
     // 'Authorization': `Bearer ${this.getAuthService().getToken()}`
     // console.error('Headders', headers);eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6Im11ZmFkZDUzQGdtYWlsLmNvbSIsIlJvbGUiOiJVc2VyIiwiZXhwIjoxNzI4MTU0NDU4LCJpc3MiOiJNeUFwcCIsImF1ZCI6Ik15QXBwVXNlcnMifQ.9pxmzrm4UgD1eS7UZysRje1Ii54sUctJMsYbeXo4MMA
 
